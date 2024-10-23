b36ec1075aaf4a5405687d4c89bbc93829bfb4d6
Omar Mneimneh <omarmneimneh2000@gmail.com>
2024-10-23 17:57:13 -0400

    Initial Commit

---
+++ CodePath Prework/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CodePath Prework
+//
+//  Created by Omar Mneimneh on 10/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+        let color = changeColor()
+        view.backgroundColor = color
+    }
+    
+}
+
